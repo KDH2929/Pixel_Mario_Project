52d7efd13f70da557ae47ba2a47a426c6d8d8a6d
wlffjt2929 <wlffjt2929@naver.com>
2024-12-01 00:02:18 +0900

    MMD IK Retargeting is finished

--- Source/PixelMario/PlayerChracter.cpp
+++ Source/PixelMario/PlayerChracter.cpp
@@ -2,8 +2,110 @@
 
 #include "PlayerChracter.h"
 #include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/Controller.h"
 
 
 APlayerChracter::APlayerChracter()
 {
+    AnimState = 0;
+}
+
+int APlayerChracter::GetAnimState()
+{
+    return AnimState;
+}
+
+void APlayerChracter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+        //KeyNum1
+        EnhancedInputComponent->BindAction(KeyNum1_Action, ETriggerEvent::Triggered, this, &APlayerChracter::PlayDance1);
+
+        //KeyNum2
+        EnhancedInputComponent->BindAction(KeyNum2_Action, ETriggerEvent::Triggered, this, &APlayerChracter::PlayDance2);
+
+        //KeyNum3
+        EnhancedInputComponent->BindAction(KeyNum3_Action, ETriggerEvent::Triggered, this, &APlayerChracter::PlayDance3);
+
+        //KeyNum4
+        EnhancedInputComponent->BindAction(KeyNum4_Action, ETriggerEvent::Triggered, this, &APlayerChracter::PlayDance4);
+
+        //KeyNum5
+        EnhancedInputComponent->BindAction(KeyNum5_Action, ETriggerEvent::Triggered, this, &APlayerChracter::PlayDance5);
+
+	}
+}
+
+void APlayerChracter::PlayDance1()
+{
+    if (AnimState == 1)
+    {
+        AnimState = 0;
+    }
+
+    else
+    {
+        AnimState = 1;
+    }
+}
+
+
+void APlayerChracter::PlayDance2()
+{
+    if (AnimState == 2)
+    {
+        AnimState = 0;
+    }
+
+    else
+    {
+        AnimState = 2;
+    }
+}
+
+
+void APlayerChracter::PlayDance3()
+{
+    if (AnimState == 3)
+    {
+        AnimState = 0;
+    }
+
+    else
+    {
+        AnimState = 3;
+    }
+}
+
+
+void APlayerChracter::PlayDance4()
+{
+    if (AnimState == 4)
+    {
+        AnimState = 0;
+    }
+
+    else
+    {
+        AnimState = 4;
+    }
+}
+
+
+void APlayerChracter::PlayDance5()
+{
+    if (AnimState == 5)
+    {
+        AnimState = 0;
+    }
+
+    else
+    {
+        AnimState = 5;
+    }
 }
--- Source/PixelMario/PlayerChracter.h
+++ Source/PixelMario/PlayerChracter.h
@@ -15,8 +15,35 @@ class PIXELMARIO_API APlayerChracter : public AThirdPersonCharacter
 public:
 	APlayerChracter();
 
+	UFUNCTION(BlueprintCallable)
+	int GetAnimState();
+
+
+protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	void PlayDance1();
+	void PlayDance2();
+	void PlayDance3();
+	void PlayDance4();
+	void PlayDance5();
+
 
 private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* KeyNum1_Action;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* KeyNum2_Action;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* KeyNum3_Action;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* KeyNum4_Action;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* KeyNum5_Action;
+
+	int AnimState;
 
 };
